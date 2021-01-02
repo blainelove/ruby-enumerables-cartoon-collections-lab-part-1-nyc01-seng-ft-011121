972095440ac3690150d9f2c398206b00cdaadb47
Blaine R Love <blainelove94@gmail.com>
2021-01-02 19:35:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index do |name, index|
-    puts "arrat[name]"
+    puts array[name]
   end
   # Use `each_with_index` to enumerate over the provided array
   #
